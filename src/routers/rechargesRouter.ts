2c4c71adda504fa666343848c7cda5e814472d43
Lucas Viana <lucasvfv@gmail.com>
2022-07-12 11:47:14 -0300

    :rocket: ready for deploy

--- src/routers/rechargesRouter.ts
+++ src/routers/rechargesRouter.ts
@@ -5,7 +5,6 @@ import { amountValidate } from "../middlewares/rechargeMiddleware.js";
 
 const rechargesRouter = Router();
 
-//TODO: validar se o funcionario pertence a empresa
 rechargesRouter.post("/recharge", tokenValidate, cardIdValidate, amountValidate, recharge);
 
 export default rechargesRouter;
